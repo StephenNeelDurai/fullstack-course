a5ffcb3c6bab447650c6703ee9ff3b71ebd2082d
StephenNeel <leadsstephen@gmail.com>
2026-02-22 21:39:41 +0530

    PATCH and DELETE

--- app.js
+++ app.js
@@ -9,14 +9,16 @@ server.listen(PORT, () => {
   console.log("Server Running on PORT", PORT);
 });
 //shalin ,10 minutes ago done
-const users = [
+let users = [
+  // array.find -> first matching value (single)
+  // array.filter ->  matching all value ([])
   // Array of users
-  // { id: 1, name: "Merciyal" },
-  // { id: 2, name: "Inthu" },
-  // { id: 3, name: "Hathija" },
-  // { id: 4, name: "Shalin" },
-  //   { id: 5, name: "Manasiga" },
-  //   { id: 6, name: "Mega" },
+  { id: 1, place: "UAE", section: "A", name: "Merciyal" },
+  { id: 2, place: "UAE", section: "A", name: "Inthu" },
+  { id: 3, place: "SPAIN", section: "A", name: "Hathija" },
+  { id: 4, place: "UAE", section: "A", name: "Shalin" },
+  { id: 5, place: "UAE", section: "A", name: "Manasiga" },
+  { id: 6, place: "SPAIN", section: "A", name: "Mega" },
 ];
 // GET / Render to the home page 2
 server.get("/", (req, res) => {
@@ -49,25 +51,43 @@ const db = {
       return users;
     },
     findById: function (id) {
-      return users.find((id) => user.id === id);
+      // array.find js
+      return users.find((user) => user.id === id);
     },
     create: function (user) {
       users.push(user);
     },
     update: function (id, name) {
-      users.find((user) => {
+      users = users.map((user) => {
         if (user.id === id) {
+          console.log("\n Updating user", id, name);
           user.name = name;
         }
-        return users;
+        return user;
       });
+    },
+    delete: function (id) {
+      users = users.filter((user) => user.id !== id);
+      return users;
     },
   },
 };
 
-server.get("/users", (req, res) => {
-  console.log("\n Get users");
+server.get("/profiles", (req, res) => {
+  console.log("\n Get users list");
   const result = db.users.findAll(); // find all (find all recoreds from user)
+  res.send({
+    staus: "success",
+    data: result,
+  });
+});
+
+server.get("/profiles/:id", (req, res) => {
+  // /profiles/1
+  console.log("\n Get single user", req.params);
+  // parseInt('1') parseFloat
+  const result = db.users.findById(parseInt(req.params.id)); // find all (find all recoreds from user)
+  console.log({ result });
   res.send({
     staus: "success",
     data: result,
@@ -78,6 +98,22 @@ server.post("/register", (req, res) => {
   const user = req.body.user; // id, name
   console.log("\n Register user", req.body.user); // Create new user
   db.users.create(user);
+  res.send({
+    staus: "success",
+  });
+});
+
+server.patch("/profiles/:id", (req, res) => {
+  console.log("\n  Update user", req.params.id, req.body.user);
+  const result = db.users.update(parseInt(req.params.id), req.body.user.name); // find all (find all recoreds from user)
+  res.send({
+    staus: "success",
+    data: result,
+  });
+});
+server.delete("/profiles/:id", (req, res) => {
+  console.log("\n Delete single user", req.params.id);
+  db.users.delete(parseInt(req.params.id)); // find all (find all recoreds from user)
   res.send({
     staus: "success",
   });
